83a5c95fca8c852e2eb7d3c9a53d79cb123088a1
Joy <joymingchen@gmail.com>
2019-06-24 17:00:24 +0800

    Add some example about initialization list

--- Dart/Part_2/initialization_list.dart
+++ Dart/Part_2/initialization_list.dart
@@ -6,12 +6,26 @@
  **/
 
 //初始化列表
-class Person{
+class Person {
   String name;
   int age;
   final String gender;
 
-  Person.withMap(Map map) : name = map["name"],gender = map["gender"]{//初始化列表
+  Person.withMap(Map map)
+      : name = map["name"],
+        gender = map["gender"] {
+    //初始化列表
     this.age = map["age"];
   }
+}
+
+class A {
+  String key;
+  String door;
+
+  A(this.key, {this.door});
+}
+
+class B extends A {
+  B(String key) : super(key,door : key);
 }
